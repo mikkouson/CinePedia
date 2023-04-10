9d1f9e8ccd42b76e5f0fa635931f268c19bbddc8
hiroo27 <lancerthortan@gmail.com>
2023-04-10 22:50:15 +0800

    added modal card

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 interface ModalProps {
   handleCloseModal: () => void;
   movie: {
+    [x: string]: string;
     title: string;
   };
 }
@@ -23,7 +24,14 @@ const Modal = ({ handleCloseModal, movie }: ModalProps) => {
       className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 backdrop-filter backdrop-grayscale z-50"
       onClick={handleCloseModal}
     >
-      <h2>{movie.title}</h2>
+      <div className=" bg-white h-[26rem] w-[50rem]">
+        {/* <h2>{movie.title}</h2> */}
+        <img
+          src={"https://image.tmdb.org/t/p/w1280" + movie.backdrop_path}
+          className=" mix-blend-multiply  "
+          alt=""
+        />
+      </div>
     </div>
   );
 };
