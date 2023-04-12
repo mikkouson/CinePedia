8bed37457fb00087d79163fc2e0c56ed6119c411
hiroo27 <lancerthortan@gmail.com>
2023-04-12 22:58:01 +0800

    added gradient

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -24,13 +24,15 @@ const Modal = ({ handleCloseModal, movie }: ModalProps) => {
       className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 backdrop-filter backdrop-grayscale z-50"
       onClick={handleCloseModal}
     >
-      <div className=" bg-white h-[26rem] w-[50rem]">
-        {/* <h2>{movie.title}</h2> */}
-        <img
-          src={"https://image.tmdb.org/t/p/w1280" + movie.backdrop_path}
-          className=" mix-blend-multiply  "
-          alt=""
-        />
+      <div className=" relative bg-white h-[26rem] w-[50rem] -z-50">
+        <h2 className="absolute">{movie.title}</h2>
+        <div className=" bg-gradient-to-r from-black to-transparent z-200 h-[26rem] w-[50rem]">
+          <img
+            src={"https://image.tmdb.org/t/p/w1280" + movie.backdrop_path}
+            className=" absolute -z-50 top-70 h-[26rem] w-[50rem]"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
