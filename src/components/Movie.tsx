39787511d176c9976b7dfcf91a681d582ff8645e
hiroo27 <lancerthortan@gmail.com>
2023-05-27 17:40:31 +0800

    Set height for movie image container

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AiFillStar } from "react-icons/ai";
 import { Link } from "react-router-dom";
+
 interface MovieProps {
   movie: {
     vote_average: number;
@@ -8,7 +9,7 @@ interface MovieProps {
     poster_path: string;
     id: number;
     title: string;
-    backdrop_path: string; // Add backdrop_path property
+    backdrop_path: string;
     overview: string;
   };
   handleOpenModal?: (movie: {
@@ -28,29 +29,33 @@ const Movie = ({ movie, handleOpenModal }: MovieProps) => {
   const handleImageLoad = () => {
     setisLoading(false);
   };
+
   return (
     <div className="flex flex-col shadow-2xl rounded-lg">
-      {isLoading && (
-        <div className="w-full h-full bg-gray-200 animate-pulse rounded-t-lg" />
+      {isLoading ? (
+        <div className="w-full h-[24rem] bg-gray-300 animate-pulse rounded-t-lg" />
+      ) : (
+        <img
+          className="w-full h-auto rounded-t-lg flex-1 cursor-pointer"
+          src={"https://image.tmdb.org/t/p/w1280" + movie.poster_path}
+          alt=""
+          onLoad={handleImageLoad}
+          onClick={() => handleOpenModal && handleOpenModal(movie)}
+        />
       )}
-      <img
-        className="w-full h-auto rounded-t-lg flex-1 cursor-pointer"
-        src={"https://image.tmdb.org/t/p/w1280" + movie.poster_path}
-        alt=""
-        onLoad={handleImageLoad}
-        onClick={() => handleOpenModal && handleOpenModal(movie)}
-      />
       <div className="p-3">
         <div className="movie" key={movie.id}></div>
-        <div className=" text-gray-600  truncate font-bold hover:text-stone-900 cursor-pointer">
+        <div className="text-gray-600 truncate font-bold hover:text-stone-900 cursor-pointer">
           <Link to={`/movie/${movie.id}`}>
-            <h4 className="Title">{movie.title}</h4>
+            <h4 className="Title whitespace-nowrap overflow-hidden text-ellipsis">
+              {movie.title}
+            </h4>
           </Link>
         </div>
         <div className="flex justify-between">
           {movie.release_date ? movie.release_date.slice(0, 4) : "Soon"}
           <div className="flex">
-            <AiFillStar className="text-yellow-500  self-center mr-2" />
+            <AiFillStar className="text-yellow-500 self-center mr-2" />
             {movie.vote_average}
           </div>
         </div>
