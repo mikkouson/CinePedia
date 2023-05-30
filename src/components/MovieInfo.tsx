db29f36b6e5e26edb6567c06113503af0fc8cdab
hiroo27 <lancerthortan@gmail.com>
2023-05-30 21:02:06 +0800

    Added movie collection

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import { AiFillStar } from "react-icons/ai";
 
 interface Genre {
   id: number;
@@ -29,6 +30,22 @@ interface Cast {
   known_for_department: string;
 }
 
+interface Parts {
+  id: number;
+  name: string;
+  vote_average: number;
+  release_date: string;
+  title: string;
+  poster_path: string;
+}
+interface Collection {
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  parts: Parts[];
+}
 interface Crew {
   id: number;
   name: string;
@@ -48,6 +65,7 @@ interface MovieProps {
   runtime: number;
   original_language: string;
   status: string;
+  belongs_to_collection: Collection;
   genres: Genre[];
   cast: Cast[];
   backdrop: Backdrop[];
@@ -62,6 +80,8 @@ const MovieInfo = () => {
   const [cast, setCast] = useState<Cast[]>([]);
   const [backdrop, setBackdrop] = useState<Backdrop[]>([]);
   const [director, setDirector] = useState<Crew[]>([]);
+  const [collection, setCollection] = useState<Collection | null>(null);
+
   useEffect(() => {
     async function getMovie() {
       const res = await fetch(
@@ -123,6 +143,25 @@ const MovieInfo = () => {
     getMovieLogo();
   }, [id]);
 
+  useEffect(() => {
+    async function getCollection() {
+      try {
+        if (movie && movie.belongs_to_collection) {
+          const res = await fetch(
+            `https://api.themoviedb.org/3/collection/${movie.belongs_to_collection.id}?api_key=3fd2be6f0c70a2a598f084ddfb75487c`
+          );
+          const data = await res.json();
+          setCollection(data);
+        }
+      } catch (error) {
+        console.error("Error fetching collection:", error);
+      }
+    }
+
+    getCollection();
+  }, [movie, id]);
+
+  console.log(collection);
   function convertTime(time: number) {
     const hours = Math.floor(time / 60);
     const minutes = time % 60;
@@ -130,11 +169,10 @@ const MovieInfo = () => {
   }
 
   if (!movie) return <div>Not Found</div>;
-
-  console.log(movie);
+  const posterUrl = `https://image.tmdb.org/t/p/w1280${collection?.backdrop_path}`;
   return (
     <main className="text-white">
-      <div className="relative">
+      <div className="relative ">
         {movie.backdrop_path ? (
           <img
             src={"https://image.tmdb.org/t/p/w1280" + movie.backdrop_path}
@@ -148,7 +186,7 @@ const MovieInfo = () => {
           />
         )}
 
-        <div className="absolute inset-x-0 bottom-0 h-20 bg-gradient-to-b from-transparent to-[#0e0e0e] z-[40]"></div>
+        <div className="absolute bottom-0 h-60 inset-x-0  bg-gradient-to-b from-transparent to-[#0e0e0e] z-[40]"></div>
       </div>
       <header className="text-white texl-xl max-w-screen-2xl mx-auto">
         <div className="absolute h-[92vh] inset-0 bg-gradient-to-r from-black to-[#00000059] "></div>
@@ -302,8 +340,8 @@ const MovieInfo = () => {
             style={{ width: "1536px" }}
             resizeObserver={false}
           >
-            {director.map((directors) => (
-              <SwiperSlide key={directors.id}>
+            {director.map((directors, index) => (
+              <SwiperSlide key={index}>
                 <div className="castContainer  mr-2">
                   <div className="box w-36 h-44 ">
                     {directors.profile_path ? (
@@ -336,10 +374,66 @@ const MovieInfo = () => {
           </Swiper>
         </div>
       </section>
+      {collection && (
+        <section
+          className="relative Collections bg-cover bg-fixed "
+          style={{ backgroundImage: `url(${posterUrl})` }}
+        >
+          <div className="absolute bottom-0  h-full inset-x-0  bg-gradient-to-b from-[#0e0e0e00] to-[#0e0e0e] z-[123]"></div>
+          <div className="absolute top-0 h-full inset-x-0  bg-gradient-to-t from-[#0e0e0e00] to-[#0e0e0e] z-[123]"></div>
 
-      <div></div>
+          <div className=" max-w-screen-2xl mx-auto py-10 flex items-center ">
+            <div className="poster w-[40rem] h-full  z-[125] ">
+              <img
+                src={`https://image.tmdb.org/t/p/w1280${collection?.poster_path}`}
+                className="w-full h-full object-cover rounded-xl"
+                alt="Profile N/A"
+              />
+            </div>
+            <div className="details  ml-5 z-[125]">
+              <h2 className="text-3xl font-bold">{collection?.name}</h2>
+              <p className="text-[#ffffffc4] font-normal text-lg">
+                {collection?.overview}
+              </p>
+              <div className="collections parts mt-5 grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 md p-2">
+                {collection?.parts.map((parts) => (
+                  <div className="castContainer  mr-2 ">
+                    <div className="box w-36 h-44 ">
+                      <img
+                        src={
+                          "https://image.tmdb.org/t/p/w1280" +
+                          parts?.poster_path
+                        }
+                        className="w-full h-full object-cover rounded-xl "
+                        alt="Profile N/A"
+                      />
+                    </div>
+                    <div className="p-3 ">
+                      <div className="text-gray-600 truncate font-bold hover:text-stone-900 cursor-pointer z-[100]">
+                        <Link to={`/movie/${parts?.id}`}>
+                          <h4 className="Title whitespace-nowrap overflow-hidden text-ellipsis text-white">
+                            {parts?.title}
+                          </h4>
+                        </Link>
+                      </div>
+                      <div className="flex justify-between text-[#ffffffc4]">
+                        {parts?.release_date
+                          ? parts?.release_date.slice(0, 4)
+                          : "Soon"}
+                        <div className="flex ">
+                          <AiFillStar className="text-yellow-500 self-center mr-2" />
+                          {parts?.vote_average}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
     </main>
   );
 };
-
 export default MovieInfo;
