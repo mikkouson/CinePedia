e7e34960abdc0d8e33b41b89d83da01a7f73477e
hiroo27 <lancerthortan@gmail.com>
2023-06-24 22:12:26 +0800

    Fixed footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full">
-      <div className="container  h-20 bg-slate-800"></div>
+    <footer className="bg-[#1a1a1b] text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <h4 className="text-xl font-bold">Cinepedia</h4>
+          <p className="text-sm">Powered by TMDB API</p>
+          <p className="text-sm">© 2023 Cinepedia. All Rights Reserved</p>
+        </div>
+        <div className="flex space-x-4">
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms of Use</a>
+        </div>
+      </div>
     </footer>
   );
 };
